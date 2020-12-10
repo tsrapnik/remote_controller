020450e766c7305ee8f9558a06a63cfa9c304f20
tsrapnik <yannick-peeters@outlook.com>
2020-12-10 20:58:21 +0100

    interpret command in http post messages.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,19 +1,23 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+
 use system_shutdown;
+use std::str;
 
-#[get("/")]
-fn hello() -> String {
-    format!("Hello!")
-}
-
-#[get("/kill")]
-fn bye() -> String {
-    system_shutdown::shutdown();
-    format!("Shutting down!")
+#[post("/", format = "text/plain", data = "<command>")]
+fn execute_command(command: rocket::Data) -> () {
+    if let Ok(command) = str::from_utf8(command.peek()) {
+        match command {
+            "shutdown" =>
+                println!("shutting down."),
+                // system_shutdown::shutdown(),
+            _ =>
+                println!("unknown command: \"{}\"", command),
+        }
+    }
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello, bye]).launch();
+    rocket::ignite().mount("/", routes![execute_command]).launch();
 }
